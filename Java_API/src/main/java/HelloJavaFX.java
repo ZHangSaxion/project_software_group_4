7be5027bd692ccf0fb7865af3b7b2d46a05a5825
KarliH <54204989+KarliH@users.noreply.github.com>
2020-11-27 15:22:33 +0100

    setting up gradle for java and javafx in folder "Java_API"

---
+++ Java_API/src/main/java/HelloJavaFX.java
@@ -0,0 +1,23 @@
+package main;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class HelloJavaFX extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        Label l = new Label("JavaFX ok.");
+        Scene scene = new Scene(new StackPane(l), 150, 100);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
