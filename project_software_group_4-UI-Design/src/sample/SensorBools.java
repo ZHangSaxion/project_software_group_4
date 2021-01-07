b952966bea373414b31e61406d161f6a736b4955
Job Gravendeel <483993@student.saxion.nl>
2021-01-07 15:57:09 +0100

    asdfas

--- project_software_group_4-UI-Design/src/sample/SensorBools.java
+++ project_software_group_4-UI-Design/src/sample/SensorBools.java
@@ -1,9 +1,45 @@
 package sample;
 
-import java.util.HashMap;
-
 public class SensorBools {
-    private static HashMap<Integer, Boolean> sensorBools;
+    private Sensor sensor;
+    private boolean temperature, pressure, ambientLight;
 
-    public static 
+    public SensorBools(Sensor sensor, boolean temperature, boolean pressure, boolean ambientLight) {
+        this.sensor = sensor;
+        this.temperature = temperature;
+        this.pressure = pressure;
+        this.ambientLight = ambientLight;
+    }
+
+    public Sensor getSensor() {
+        return sensor;
+    }
+
+    public void setSensor(Sensor sensor) {
+        this.sensor = sensor;
+    }
+
+    public boolean isTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(boolean temperature) {
+        this.temperature = temperature;
+    }
+
+    public boolean isPressure() {
+        return pressure;
+    }
+
+    public void setPressure(boolean pressure) {
+        this.pressure = pressure;
+    }
+
+    public boolean isAmbientLight() {
+        return ambientLight;
+    }
+
+    public void setAmbientLight(boolean ambientLight) {
+        this.ambientLight = ambientLight;
+    }
 }
