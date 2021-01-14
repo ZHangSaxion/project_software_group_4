2201ff7ead41e5f279e441184c89d99d4c40db9c
KarliH <54204989+KarliH@users.noreply.github.com>
2021-01-14 09:43:25 +0100

    remove MQTT files

--- Java_API/src/main/java/APIController.java
+++ Java_API/src/main/java/APIController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.text.SimpleDateFormat;
-import java.util.*;
 import java.util.concurrent.atomic.AtomicReference;
 
 /**
